f7ed5bcd586f18b3b3ceb5b931deb5987ee89cad
lalankeba <lalankeba@gmail.com>
2023-12-20 11:55:05 +0200

    adding initial source code

---
+++ src/test/java/com/laan/devopsapp/DevopsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laan.devopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
