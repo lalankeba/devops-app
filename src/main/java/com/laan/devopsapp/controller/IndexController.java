20fcdd819cb076e793be233d4d6cc29bf1e06286
lalankeba <lalankeba@gmail.com>
2023-12-20 14:05:52 +0200

    add welcome service

---
+++ src/main/java/com/laan/devopsapp/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.laan.devopsapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+@Slf4j
+public class IndexController {
+
+    @GetMapping
+    public ResponseEntity<Object> getDepartment() {
+        log.info("initializing app");
+        Map<String, Object> map = Collections.singletonMap("message", "Success");
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/laan/devopsapp/filter/MdcFilter.java
@@ -0,0 +1,34 @@
+package com.laan.devopsapp.filter;
+
+import com.laan.devopsapp.util.IdGenUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+@RequiredArgsConstructor
+public class MdcFilter extends OncePerRequestFilter {
+
+    private static final String MDC_KEY = "mdcId";
+
+    private final IdGenUtil idGenUtil;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain chain) throws ServletException, IOException {
+        String mdcId = idGenUtil.get20LengthId();
+        MDC.put(MDC_KEY, mdcId);
+        chain.doFilter(request, response);
+        MDC.remove(MDC_KEY);
+    }
+}
